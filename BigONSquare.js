0199643437a879b1acadc61d45d684531c0a261b
Rishabh157 <rishabhgour157@gmail.com>
2024-01-09 00:40:32 +0530

    added consucative with o(n^2)

---
+++ BigONSquare.js
@@ -0,0 +1,17 @@
+
+// 
+
+function BigONSquare(n) {
+
+    for (let i = 0; i < n; i++) {
+
+        for (let j = 0; j < n; j++) {
+            console.log(i, j)
+        }
+
+    }
+}
+
+BigONSquare(4)
+
+// quadritic => o(n2)
--- Consecutive.js
+++ Consecutive.js
@@ -36,3 +36,37 @@ function largestNumber(array, consucativeNumber) {
 }
 
 largestNumber([1, 2, 3, 4, 3, 5, 4, 6, 7, 8], 4)
+
+
+// ----------------------------------------------------------
+
+// ANS -> [23, 7, 8, 100]  =  42
+// O(n^2)
+
+function consucativeNumberWithQuadrtive(array, consucativeNumber) {
+
+    if (!array.length) {
+        throw Error("Array is empty")
+    }
+
+    let totalSum = 0;
+
+    for (let i = 0; i <= array.length - consucativeNumber; i++) {
+
+        let total = 0;
+
+        for (let j = i; j < consucativeNumber + i; j++) {
+            total += array[j]
+        }
+
+        if (totalSum < total) {
+            totalSum = total
+        }
+
+
+    }
+    console.log('totalSum', totalSum)
+
+}
+
+consucativeNumberWithQuadrtive([2, 8, 5, 6, 3, 4, 23, 7, 8, 100, 1, 1, 3, 2], 4)
