26d280b0e3674d0145022940048ec3c690ce634e
Rishabh157 <rishabhgour157@gmail.com>
2024-01-12 00:54:23 +0530

    add liner search

---
+++ LinerSearch.js
@@ -0,0 +1,21 @@
+// Liner Search Algorithm
+// Time Complxity - liner o(n)
+
+let users = [
+    {
+        email: 'rishabhgour157@gmail.com',
+        password: 'Password1!'
+    },
+    {
+        email: 'dheeraj123@gmail.com',
+        password: 'Password1!'
+    },
+    {
+        email: 'nikhil123@gmail.com',
+        password: 'Password1!'
+    }
+]
+
+for (let item of users) {
+    console.log(item['email'])
+}
