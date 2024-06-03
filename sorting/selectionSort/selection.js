a402fe7d513ba95e81dd65b345880632256cdb5e
Rishabh157 <rishabhgour157@gmail.com>
2024-06-04 00:47:33 +0530

    add the selection sort algo via js with redme file :-)

--- sorting/selectionSort/selection.js
+++ sorting/selectionSort/selection.js
@@ -1,20 +1,14 @@
-
 function selectionSort(array) {
-
     for (let i = 0; i < array.length; i++) {
 
         let min = i
 
         for (let j = i + 1; j < array.length; j++) {
-
             if (array[min] > array[j]) {
                 min = j
             }
-
         }
-
         [array[i], array[min]] = [array[min], array[i]]
-
     }
     return array
 }
