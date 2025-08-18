8220f3a195aeb198b1914048efe4c5862cd04a65
Rishabh157 <rishabhgour157@gmail.com>
2025-08-18 19:58:14 +0530

    added the checking sum zero problem with function

--- CheckingSumZero.js
+++ CheckingSumZero.js
@@ -1,34 +1,45 @@
 
-// Q ->  Checking Sum zero 
+// Q ->  Checking Sum zero and return the first pair 
 // [-5 , -4 , -3 , -2 , 0 , 2 , 4 , 6 , 8]
+// ans -> [-4 , 4]
 
 const arr = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
 
-
-
 // first solution with two loops
-for (let i in arr) {
-    for (let j = 1; j < arr.length; j++) {
-        if (arr[i] + arr[j] === 0) {
-            return console.log([arr[i], arr[j]])
+function findZeroPairWithTwoLoops(arr) {
+
+    for (let i in arr) {
+        for (let j = 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === 0) {
+                return console.log([arr[i], arr[j]])
+            }
         }
     }
 }
 
+// call the function
+findZeroPairWithTwoLoops(arr);
+
+
 // second solution with one loop
-let i = 0;
-let j = 1;
+function findZeroPairWithOneLoop(arr) {
 
-let isBreack = true
+    let i = 0;
+    let j = 1;
+    let isBreak = true
 
-while (isBreack) {
-
-    if (arr[i] + arr[j] === 0) {
-        console.log([arr[i], arr[j]]);
-        isBreack = false
-    } else if (j <= arr.length) {
-        i++
-    } else {
-        j++
+    while (isBreak) {
+        if (arr[i] + arr[j] === 0) {
+            console.log([arr[i], arr[j]]);
+            isBreak = false
+        } else if (j <= arr.length) {
+            i++
+        } else {
+            j++
+        }
     }
+
 }
+
+// call the function
+findZeroPairWithOneLoop(arr);
